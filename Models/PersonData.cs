6140f53b5ff4733273734592ac1f5bfc371e1b2a
Anton Vorobiov <kriksalisprogramming@gmail.com>
2026-08-09 23:53:47 +0300

    Fix gitignore and update models rule

---
+++ Models/PersonData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ML.Data;
+namespace Mathine_test.Models;
+
+public class PersonData
+{
+    
+    [LoadColumn(0)]
+    public float Height { get; set; }
+    [LoadColumn(1)]
+    public float Weight { get; set; }
+    [LoadColumn(2)] 
+    public bool isAtlete { get; set; }
+
+    [LoadColumn(3)] 
+    public string result { get; set; } = "";
+    [LoadColumn(4)]
+    public float RandomFeature { get; set; }
+    [LoadColumn(5)]
+    public float BMI { get; set; }
+}
+
+public class PersonPrediction
+{
+    [ColumnName("PredictedLabel")] public string PredictedResult { get; set; } = "";
+    public float[] Score { get; set; } = [];
+}
